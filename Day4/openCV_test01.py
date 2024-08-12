9397f619ac6c63ac6acda63e3aacd4a03de0e040
Python-project <162897281+python-Moon@users.noreply.github.com>
2024-08-13 00:20:35 +0900

    Update openCV_test01.py

--- Day4/openCV_test01.py
+++ Day4/openCV_test01.py
@@ -3,16 +3,17 @@ import cv2
 from matplotlib import pyplot as plt
 image_path= 'chungun.png'
 image = cv2.imread(image_path)
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 
 filter = np.array([
     [1, 1, 1],
-    [1, -10, 1],
+    [1, -5, 1],
     [1, 1, 1]
 ])
 
-feature_map = cv2.filter2D(image, -1, filter)
+feature_map = cv2.filter2D(image_rgb, -1, filter)
 
-plt.subplot(121), plt.imshow(image), plt.title('Original Image')
+plt.subplot(121), plt.imshow(image_rgb), plt.title('Original Image')
 plt.subplot(122), plt.imshow(feature_map), plt.title('Feature Map')
 plt.show()
